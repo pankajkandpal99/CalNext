bdaa38d2b67f0a423d892cfab8d8263ec4f7f90f
pankajkandpal <kandpalpankaj34@gmail.com>
2024-12-02 15:36:23 +0530

    update

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,14 +1,30 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  var prismaGlobal: PrismaClient | undefined;
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const prisma =
+  global.prismaGlobal ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // Optional: Prisma logs for debugging
+  });
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV !== "production") global.prismaGlobal = prisma;
+
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
